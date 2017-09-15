0c699a133854a01a9d04d6ce0159f77256f05a1c
Robert Romano <rromano@gmail.com>
2017-09-14 22:40:36 -0400

    Update for Django >= 1.8

--- adminrestrict/test_settings.py
+++ adminrestrict/test_settings.py
@@ -11,6 +11,28 @@ if django.VERSION[:2] >= (1, 3):
 else:
     DATABASE_ENGINE = 'sqlite3'
 
+if django.VERSION[:2] >= (1, 8):    
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                ],
+                'loaders':[
+                    ('django.template.loaders.cached.Loader', [
+                        'django.template.loaders.filesystem.Loader',
+                        'django.template.loaders.app_directories.Loader',
+                    ]),
+                ],
+            },
+        },
+    ]
+
 SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
@@ -31,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'adminrestrict',
 ]
+
 
 SECRET_KEY = 'too-secret-for-test'
 
--- adminrestrict/test_urls.py
+++ adminrestrict/test_urls.py
@@ -1,6 +1,14 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-)
+try:
+    from django.conf.urls import patterns
+    urlpatterns = patterns('',
+        url(r'^admin/', include(admin.site.urls)),
+    )
+except ImportError:
+    urlpatterns = [
+        url(r'^admin/', include(admin.site.urls)),
+    ]
+
+
