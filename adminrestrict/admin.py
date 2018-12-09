c241324867bd3e4eded46e039fc4dbbdc7456bca
lmarvaud <24732919+lmarvaud@users.noreply.github.com>
2018-12-09 16:21:11 +0100

    feat: auto fill creator and editor on model saving
    Signed-off-by: lmarvaud <24732919+lmarvaud@users.noreply.github.com>

--- adminrestrict/admin.py
+++ adminrestrict/admin.py
@@ -12,7 +12,15 @@ from adminrestrict.models import AllowedIP
 
 class AllowedIPAdmin(admin.ModelAdmin):
     list_display = ('id', 'ip_address', 'created_by', 'edited_by')
+    list_editable = ('ip_address', 'description')
     readonly_fields = ('created_by', 'edited_by')
+
+    def save_model(self, request, instance, form, change):
+        user = request.user
+        if not change or not instance.created_by:
+            instance.created_by = user
+        instance.edited_by = user
+        return super(AllowedIPAdmin, self).save_model(request, instance, form, change)
 
 admin.site.register(AllowedIP, AllowedIPAdmin)
 
