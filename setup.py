3da8ccf7d2de5239f7dfc7cf6639eb89456d9b2e
Robert C. Romano <robromano@users.noreply.github.com>
2022-07-31 10:08:28 -0700

    Update setup.py version number, drop Python 2.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ HERE = os.path.realpath(os.path.join(__file__, '..'))
 # The text of the README file
 README = open(os.path.join(HERE,"README.md")).read()
 
-VERSION = '3.0'
+VERSION = '3.1'
 
 setup(
     name='django-adminrestrict',
@@ -35,7 +35,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: Log Analysis',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
