0c134fafda9d975df845ce6afb3f737f1738b32a
Jay Ponce <github.tech.accounts@jayponce.com>
2022-10-23 00:21:32 -0500

    Fixed GET test for custom denied templates

--- adminrestrict/tests.py
+++ adminrestrict/tests.py
@@ -97,7 +97,7 @@ class BasicTests(TestCase):
             post_response = self.client.post("/admin/", data={'username':"foo", 'password':"bar"}, follow=True)
             self.assertEqual(post_response.status_code, 403)
             self.assertEqual(post_response.content, DENIED_TEMPLATE)
-            get_response = self.client.get("/admin/")
+            get_response = self.request.get("/admin/")
             self.assertEqual(get_response.status_code, 403)
             self.assertEqual(get_response.content, DENIED_TEMPLATE)
         a.delete()
